804a062208197de71cec1a39fdba7f496bede09c
onigirizoro108 <suguru_kitagaki@mba.berkeley.edu>
2014-02-23 09:58:58 +0000

    task1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ageiapp1::Application.config.secret_key_base = '4840190d17d4f76b580293c58aca224a29f3e46e3416c285cec2538282bd333379f5e1c0b90867a860e150accd65ac37f45c715dbb88d31bf4b79cf50d4c4d0b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ageiapp1::Application.config.session_store :cookie_store, key: '_ageiapp1_session'
